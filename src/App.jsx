e68e57d20a1357bb88804386f634a7517113a59d
Sajib Khan <sajibkhan.mern@gmail.com>
2023-10-29 13:09:22 +0600

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h2>This is Tronix Commerce Frontend</h2>
+    <h2 className='bg-purple-500 text-2xl'>This is Tronix Commerce Frontend</h2>
   )
 }
 
